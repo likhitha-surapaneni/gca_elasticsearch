526539252cd5e8685887d9ab9f3ed073763215df
streeter <streeter@ebi.ac.uk>
2014-12-25 18:38:10 +0000

    proxy is better

--- scripts/proxy.pl
+++ scripts/proxy.pl
@@ -12,6 +12,7 @@ my $daemon = HTTP::Daemon->new(LocalAddr => 'localhost', LocalPort => 8200) or d
 print "Please contact me at: <URL:", $daemon->url, ">\n";
 my $elastic_search_server = 'localhost:9200';
 my $elastic_search = LWP::UserAgent->new();
+my $read_size = 1000000;
 
 while (my $connection = $daemon->accept) {
   REQUEST:
@@ -25,10 +26,25 @@ while (my $connection = $daemon->accept) {
     $uri->host_port($elastic_search_server);
     $uri->scheme('http');
 
+    my $request_bytes_read = 0;
+    my $request_content_length = $request->content_length;
     my $es_request = HTTP::Request->new($request->method, $uri, $request->headers);
-    if ($request->headers->{'Content-Length'}) {
-        $es_request->content(sub {return $connection->read_buffer});
-    }
+    $es_request->content(sub {
+        if ($request_bytes_read >= $request_content_length) {
+            return '';
+        }
+        if (my $read_buffer = $connection->read_buffer) {
+            $connection->read_buffer('');
+            $request_bytes_read += length($read_buffer);
+            return $read_buffer;
+        }
+        else {
+            my $received;
+            $connection->recv($received, $read_size);
+            $request_bytes_read += length($read_buffer);
+            return $received;
+        }
+    });
 
     my $is_header_sent = 0;
     my $es_response = $elastic_search->request($es_request, sub {
@@ -40,7 +56,7 @@ while (my $connection = $daemon->accept) {
                 $is_header_sent = 1;
             }
             $connection->send($data);
-      });
+      }, $read_size);
 
   }
   $connection->close();
