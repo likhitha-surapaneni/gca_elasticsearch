f43ca1ba2c05b0fd5ef6a8c7f43ca49218f46add
streeter <streeter@ebi.ac.uk>
2016-01-18 15:50:59 +0000

    Put a condition on the redirection route, so it only gets routed if there is a real redirection

--- lib/ReseqTrack/ElasticsearchProxy/Plugins/Redirect.pm
+++ lib/ReseqTrack/ElasticsearchProxy/Plugins/Redirect.pm
@@ -33,6 +33,7 @@ sub refresh_redirection_hash {
 
 sub register {
     my ($self, $app, $args) = @_;
+    $app->plugin('DefaultHelpers');
     my $file = $args->{file};
 
     $self->refresh_redirection_hash($file);
@@ -41,12 +42,18 @@ sub register {
         300 => sub { $self->refresh_redirection_hash($file)}
     );
 
-    #$app->hook( before_routes => sub {
-    $app->routes->any('/*')->to(cb => sub {
-        my ($controller) = @_;
+    $app->routes->add_condition('has_redirect' => sub {
+        my ($route, $controller, $captures) = @_;
         if (my $redirect = $self->{redirection_hash}{Mojo::Util::url_unescape( $controller->req->url->path )}) {
-            $controller->redirect_to($redirect);
+            $controller->stash(redirect_to => $redirect);
+            return 1;
         }
+        return undef;
+    });
+
+    $app->routes->any('/*')->over('has_redirect' => 1)->to(cb => sub {
+        my ($controller) = @_;
+        $controller->redirect_to($controller->stash('redirect_to');
     });
 }
 
