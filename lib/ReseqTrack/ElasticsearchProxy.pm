85788402ebcd87f18a43a5920866011032a456ba
streeter <streeter@ebi.ac.uk>
2016-06-08 11:57:00 +0100

    Moved the API routing to a separate plugin

--- lib/ReseqTrack/ElasticsearchProxy.pm
+++ lib/ReseqTrack/ElasticsearchProxy.pm
@@ -11,30 +11,9 @@ sub startup {
     }
 
     if (my $api_routes = $self->config('api_routes')) {
-      my $api = $self->routes->under('/' => sub {
-        my ($controller) = @_;
-        API_ROUTE:
-        foreach my $api_route (@$api_routes) {
-          my ($api_path, $es_path) = @{$api_route}{qw(from to)};
-          next API_ROUTE if !$controller->req->url->path->contains($api_path);
-          my $req_path = $controller->req->url->path->to_string;
-          $req_path =~ s{^$api_path}{/$es_path/};
-          $req_path =~ s{//+}{/}g;
-          if ($req_path =~ s/\.(\w+)$//) {
-            $controller->stash(format => $1);
-          }
-          $controller->stash(es_path => $req_path);
-          return 1;
-        }
-      });
-      foreach my $api_route (@$api_routes) {
-        my $api_path = $api_route->{from};
-        $api->to(controller => 'elasticsearch');
-        $api->route($api_path)->via('GET', 'POST')->to(action => 'es_query_router');
-        $api->route("$api_path/*")->via('GET', 'POST')->to(action => 'es_query_router');
-        $api->route($api_path)->to(action=>'method_not_allowed');
-        $api->route("$api_path/*")->to(action=>'method_not_allowed');
-      }
+      $self->plugin('ReseqTrack::ElasticsearchProxy::Plugins::API',
+        routes => $api_routes,
+      );
     }
 
     my $static_dirs = $self->config('static_directories') || [];
---
+++ lib/ReseqTrack/ElasticsearchProxy/Plugins/API.pm
@@ -0,0 +1,34 @@
+package ReseqTrack::ElasticsearchProxy::Plugins::API;
+use Mojo::Base qw{ Mojolicious::Plugin };
+
+sub register {
+    my ($self, $app, $args) = @_;
+
+    my $api = $app->routes->under('/' => sub {
+      my ($controller) = @_;
+      API_ROUTE:
+      foreach my $api_route (@{$args->{routes}}) {
+        my ($api_path, $es_path) = @{$api_route}{qw(from to)};
+        next API_ROUTE if !$controller->req->url->path->contains($api_path);
+        my $req_path = $controller->req->url->path->to_string;
+        $req_path =~ s{^$api_path}{/$es_path/};
+        $req_path =~ s{//+}{/}g;
+        if ($req_path =~ s/\.(\w+)$//) {
+          $controller->stash(format => $1);
+        }
+        $controller->stash(es_path => $req_path);
+        return 1;
+      }
+    });
+    foreach my $api_route (@{$args->{routes}}) {
+      my $api_path = $api_route->{from};
+      $api->to(controller => 'elasticsearch');
+      $api->route($api_path)->via('GET', 'POST')->to(action => 'es_query_router');
+      $api->route("$api_path/*")->via('GET', 'POST')->to(action => 'es_query_router');
+      $api->route($api_path)->to(action=>'method_not_allowed');
+      $api->route("$api_path/*")->to(action=>'method_not_allowed');
+    }
+
+}
+
+1;
