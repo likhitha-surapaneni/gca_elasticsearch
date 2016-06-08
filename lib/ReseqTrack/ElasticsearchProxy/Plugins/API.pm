eccead27490de51229456794bb31d2725bf1c305
streeter <streeter@ebi.ac.uk>
2016-06-08 18:22:40 +0100

    Code elegance: merged two for loops into one

--- lib/ReseqTrack/ElasticsearchProxy/Plugins/API.pm
+++ lib/ReseqTrack/ElasticsearchProxy/Plugins/API.pm
@@ -4,12 +4,11 @@ use Mojo::Base qw{ Mojolicious::Plugin };
 sub register {
     my ($self, $app, $args) = @_;
 
-    my $api = $app->routes->under('/' => sub {
-      my ($controller) = @_;
-      API_ROUTE:
-      foreach my $api_route (@{$args->{routes}}) {
-        my ($api_path, $es_path) = @{$api_route}{qw(from to)};
-        next API_ROUTE if !$controller->req->url->path->contains($api_path);
+    foreach my $api_route (@{$args->{routes}}) {
+      my ($api_path, $es_path) = @{$api_route}{qw(from to)};
+      my $api = $app->routes->under($api_path => sub {
+        my ($controller) = @_;
+        return undef if !$controller->req->url->path->contains($api_path);
         my $req_path = $controller->req->url->path->to_string;
         $req_path =~ s{^$api_path}{/$es_path/};
         $req_path =~ s{//+}{/}g;
@@ -18,16 +17,14 @@ sub register {
         }
         $controller->stash(es_path => $req_path);
         return 1;
-      }
-    } => {
-      es_host => $args->{es_host},
-      es_port => $args->{es_port}
-    });
-    $api->to(controller => 'elasticsearch');
-    foreach my $api_route (@{$args->{routes}}) {
-      my $api_path = $api_route->{from};
-      $api->any(['GET', 'POST'], "$api_path/*whatever" => {whatever => ''})->to(action => 'es_query_router');
-      $api->any("$api_path/*whatever")->to(action=>'method_not_allowed');
+      } => {
+        es_host => $args->{es_host},
+        es_port => $args->{es_port},
+      });
+
+      $api->to(controller => 'elasticsearch');
+      $api->any(['GET', 'POST'], '/*whatever' => {whatever => ''})->to(action => 'es_query_router');
+      $api->any('/*whatever')->to(action=>'method_not_allowed');
     }
 
 }
