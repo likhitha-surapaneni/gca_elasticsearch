9b7ab3a7bd945fb65aad88df69d73c3f9485b3a1
streeter <streeter@ebi.ac.uk>
2016-06-08 12:04:10 +0100

    Moved Angularjs routing to a separate plugin

--- lib/ReseqTrack/ElasticsearchProxy.pm
+++ lib/ReseqTrack/ElasticsearchProxy.pm
@@ -19,54 +19,16 @@ sub startup {
     my $static_dirs = $self->config('static_directories') || [];
     push @{$self->static->paths}, @$static_dirs;
 
-    my $angularjs_apps = $self->config('angularjs_apps') || [];
-    my $angularjs_html5_apps = $self->config('angularjs_html5_apps') || [];
-    foreach (@$angularjs_apps, @$angularjs_html5_apps) {
-      s{/*$}{/};
-      s{^/*}{/};
-    }
-
-    foreach my $app_home (@$angularjs_html5_apps) {
-      $self->routes->get($app_home.'*' => sub {
-        my ($c) = @_;
-        return if !$c->accepts('html');
-        $c->reply->static($app_home.'index.html');
-        $c->res->headers->cache_control('max_age=1, no_cache');
-      });
-    }
-
-    foreach my $app_home (@{$angularjs_apps}) {
-      $self->routes->get($app_home => sub {
-        my ($c) = @_;
-        return if !$c->accepts('html');
-        my $req_path = $c->req->url->path;
-        if (!$req_path->trailing_slash) {
-          $c->res->code(301);
-          $req_path->trailing_slash(1);
-          return $c->redirect_to($req_path->to_string);
-        }
-        $c->reply->static($app_home.'index.html');
-        $c->res->headers->cache_control('max_age=1, no_cache');
-      });
-    }
-
-    if (@$angularjs_apps || @$angularjs_html5_apps) {
-      $self->hook(after_static => sub {
-        my ($c) = @_;
-        my $req_path = $c->req->url->path->to_string;
-        if (grep {$req_path eq $_.'index.html'} @$angularjs_apps, @$angularjs_html5_apps) {
-          return $c->res->headers->cache_control('max_age=1, no_cache');
-        }
-      });
-    }
+    $self->plugin('ReseqTrack::ElasticsearchProxy::Plugins::AngularJS',
+      angularjs_apps => $self->config('angularjs_apps'),
+      angularjs_html5_apps => $self->config('angularjs_html5_apps'),
+    );
 
     $self->routes->get('/*whatever' => {whatever => ''} => sub {
       my ($c) = @_;
       return if !$c->accepts('html');
       $c->reply->static($c->stash('whatever') . '/index.html');
     });
-
-
 
     if (my $redirect_file = $self->config('redirect_file')) {
         $self->plugin('ReseqTrack::ElasticsearchProxy::Plugins::Redirect', file => $redirect_file);
