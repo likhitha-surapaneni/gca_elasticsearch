5e23ea303ef65a6cb7138995ca0d3128d1825d41
streeter <streeter@ebi.ac.uk>
2016-06-10 06:50:00 +0100

    Some in line documentation

--- lib/ReseqTrack/ElasticsearchProxy.pm
+++ lib/ReseqTrack/ElasticsearchProxy.pm
@@ -10,10 +10,14 @@ sub startup {
       $self->log->path($log_file);
     }
 
+    # Enable cross-origin research sharing
+    # Read more about it: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+    # Good for a sharable API
     if ($self->config('cors.enabled')) {
         $self->plugin('CORS');
     }
 
+    # This plugin does all the routing for the elasticsearch API
     if (my $api_routes = $self->config('api_routes')) {
       $self->plugin('ReseqTrack::ElasticsearchProxy::Plugins::API',
         routes => $api_routes,
@@ -22,14 +26,21 @@ sub startup {
       );
     }
 
-    my $static_dirs = $self->config('static_directories') || [];
-    push @{$self->static->paths}, @$static_dirs;
+    # Files in these static directories will get served directly
+    if (my $static_dirs = $self->config('static_directories')) {
+      push @{$self->static->paths}, @$static_dirs;
+    }
 
+    # This plugin makes sure angularjs apps are routed correctly
+    # It makes sure index.html files are not cached.
+    # use the angularjs_html5_apps array if the app uses angularjs option $location.html5Mode(true)
+    # otherwise use the angularjs_apps array
     $self->plugin('ReseqTrack::ElasticsearchProxy::Plugins::AngularJS',
       angularjs_apps => $self->config('angularjs_apps'),
       angularjs_html5_apps => $self->config('angularjs_html5_apps'),
     );
 
+    # This is to make sure paths matching a directory serve the index.html file
     $self->routes->get('/*whatever' => {whatever => ''} => sub {
       my ($c) = @_;
       return if !$c->accepts('html');
